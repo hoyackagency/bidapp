33db7c302883970fbe38d94009b32db871c5e63a
Brandon Cross <brandon@hoyack.com>
2023-07-22 07:56:48 -0500

    created static js and css for webapp template

--- webapp/static/webapp/js/main.js
+++ webapp/static/webapp/js/main.js
@@ -1,42 +1,40 @@
-// myproject/webapp/static/webapp/js/main.js
+// main.js
 
-document.addEventListener('DOMContentLoaded', function() {
-    var feed_entry_id = document.getElementById('data-id').textContent;
+function sendStatus(status) {
+    fetch(thumbs_down_url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: `id=${feed_entry_id}&status=${status}`,
+    }).then(response => {
+        console.log(response);
+        location.reload();  // reload the page
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+    });
+}
 
-    function sendStatus(status) {
-        fetch('/thumbsdown/', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded',
-            },
-            body: `id=${feed_entry_id}&status=${status}`,
-        }).then(response => {
-            console.log(response);
-            location.reload();  // reload the page
+document.getElementById("thumbsdown").onclick = function() {
+    sendStatus('decline');
+}
+
+document.getElementById("thumbsup").onclick = function() {
+    sendStatus('accept');
+}
+
+document.getElementById("recycle").onclick = function() {
+    fetch(`http://localhost:8000/view?recycle=true&feed_entry_id=${feed_entry_id}`)
+        .then(response => response.json())
+        .then(data => {
+            var titleElement = document.getElementById('card-title');
+            titleElement.textContent = data.title;
+            titleElement.href = data.link;  // set the href to the new URL
+            document.getElementById('card-content').textContent = data.content;
+            feed_entry_id = data.id;  // update feed entry id
         })
         .catch((error) => {
             console.error('Error:', error);
         });
-    }
-
-    document.getElementById("thumbsdown").onclick = function() {
-        sendStatus('decline');
-    }
-
-    document.getElementById("thumbsup").onclick = function() {
-        sendStatus('accept');
-    }
-
-    document.getElementById("recycle").onclick = function() {
-        fetch(`http://localhost:8000/view?recycle=true&feed_entry_id=${feed_entry_id}`)
-            .then(response => response.json())
-            .then(data => {
-                document.getElementById('card-title').textContent = data.title;
-                document.getElementById('card-content').textContent = data.content;
-                feed_entry_id = data.id;  // update feed entry id
-            })
-            .catch((error) => {
-                console.error('Error:', error);
-            });
-    }
-});
+}
