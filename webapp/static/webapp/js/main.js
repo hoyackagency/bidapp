ba6d4753c32501a60e8acae130d3568f75a038a0
Brandon Cross <brandon@hoyack.com>
2023-07-22 20:53:40 -0500

    implemented bootstrap pill badge for skills list

--- webapp/static/webapp/js/main.js
+++ webapp/static/webapp/js/main.js
@@ -24,23 +24,31 @@ document.getElementById("thumbsup").onclick = function() {
     sendStatus('accept');
 }
 
-document.getElementById("recycle").onclick = function() {
-    fetch(`http://localhost:8000/view?recycle=true&feed_entry_id=${feed_entry_id}`)
-        .then(response => response.json())
-        .then(data => {
-            document.getElementById('card-title').textContent = data.title;
-            document.getElementById('card-title').href = data.link;
-            document.getElementById('card-content').textContent = data.content;
-            document.getElementById('card-feed').textContent = "Feed: " + data.feed.feed_name;
-            document.getElementById('card-published_date').textContent = "Published Date: " + data.published_date;
-            document.getElementById('card-pay_range').textContent = "Pay Range: " + data.pay_range;
-            document.getElementById('card-job_type').textContent = "Job Type: " + data.job_type;
-            document.getElementById('card-category').textContent = "Category: " + data.category;
-            document.getElementById('card-skills').textContent = "Skills: " + data.skills;
-            document.getElementById('card-country').textContent = "Country: " + data.country;
-            feed_entry_id = data.id;
-        })
-        .catch((error) => {
-            console.error('Error:', error);
-        });
+document.getElementById("recycle-btn").onclick = function() {
+    fetch(`/view?recycle=true&feed_entry_id=${feed_entry_id}`)
+    .then(response => response.json())
+    .then(data => {
+        feed_entry_id = data.id;  // update the global variable with the new id
+
+        // update the HTML elements with the new data
+        document.getElementById("card-title").textContent = data.title;
+        document.getElementById("card-content").textContent = data.content;
+        document.getElementById("card-feed").textContent = "Feed: " + data.feed.feed_name;
+        // document.getElementById("card-published_date").textContent = "Published Date: " + data.published_date;
+        document.getElementById("card-pay_range").textContent = "Pay Range: " + data.pay_range;
+        document.getElementById("card-job_type").textContent = "Job Type: " + data.job_type;
+        document.getElementById("card-category").textContent = "Category: " + data.category;
+        
+        // split the skills string into an array of skills
+        let skillsArray = data.skills.split(',');
+        // create a badge for each skill
+        let badgesHTML = skillsArray.map(skill => `<span class="badge badge-pill badge-primary">${skill.trim()}</span>`).join(' ');
+        // set the HTML of the skills element to the badges
+        document.getElementById("card-skills").innerHTML = "Skills: " + badgesHTML;
+        
+        document.getElementById("card-country").textContent = "Country: " + data.country;
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+    });
 }
