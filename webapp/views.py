eff478bbfe113a1bb7841d807a76d249d0c49ef8
Brandon Cross <brandon@hoyack.com>
2023-07-23 14:03:30 -0500

    updated view for end of list case and added template

--- webapp/views.py
+++ webapp/views.py
@@ -8,7 +8,12 @@ from django.views.decorators.csrf import csrf_exempt
 def index(request):
     response = requests.get('http://localhost:8000/view')
     data = response.json()
-    return render(request, 'webapp/index.html', {'data': data})
+#    return render(request, 'webapp/index.html', {'data': data})
+    if data.get('id') == 0:
+        # if id is 0, render a different template
+        return render(request, 'webapp/end_of_list.html')
+    else:
+        return render(request, 'webapp/index.html', {'data': data})
 
 @csrf_exempt
 def thumbs_down(request):
